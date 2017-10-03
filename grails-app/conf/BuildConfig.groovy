3386faa54f398428839bd537ff5ac74fa409fed4
Dave Martin <djtfmartin@users.noreply.github.com>
2017-10-03 06:14:10 -0400

    1.7.2 plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build ":release:3.0.1"
-        runtime ":collectory:1.7"
+        runtime ":collectory:1.7.2"
         build ":tomcat:7.0.54"
     }
 }
